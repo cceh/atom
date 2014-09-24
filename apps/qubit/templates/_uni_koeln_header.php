d1a61ac366854f7079fa6285f63edb1b86eb05f4
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2014-09-24 17:39:31 +0200

    MEA: Add links to various pages in header

--- apps/qubit/templates/_uni_koeln_header.php
+++ apps/qubit/templates/_uni_koeln_header.php
@@ -2,7 +2,7 @@
 	<div id="metaNavigation">
 		<ul class="list-inline" id="languageSwitch">
 			<li>
-				<a href="">DATA CENTER FOR THE HUMANITIES</a>
+				<a href="http://dch.uni-koeln.de">Data Center for the Humanities</a>
 			</li>
 			<li>|</li>
 			<li>
@@ -15,12 +15,12 @@
 	</div>
 	<div id="mainLogo">
 		<a href="http://www.uni-koeln.de">
-			<?php echo image_tag('/images/UzK_Logo.png', 'alt="Universität zu Köln"') ?>	
+			<?php echo image_tag('/images/UzK_Logo.png', 'alt="Universität zu Köln"') ?>
 			<span class="hidden-text">Universität zu Köln</span>
 		</a>
 	</div>
 	<div id="mainHeading">
-		<div class="sub-header heading">Universität zu Köln &bull; Thomas-Institut</div>
-		<h1 class="main-header heading">Meister-Eckhart-Archiv</h1>
+		<div class="sub-header heading">Universität zu Köln &bull; <a href="http://www.thomasinstitut.uni-koeln.de/">Thomas-Institut</a></div>
+		<h1 class="main-header heading"><a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">Meister-Eckhart-Archiv</a></h1>
 	</div>
 </div>
