0d66191e7cf8c82764ae43a043831703aa1850d1
Bernhard <bernhard.strecker@uni-koeln.de>
2015-07-02 19:35:16 +0200

    MEA: Make notes publicly available

--- plugins/sfIsadPlugin/modules/sfIsadPlugin/templates/indexSuccess.php
+++ plugins/sfIsadPlugin/modules/sfIsadPlugin/templates/indexSuccess.php
@@ -199,8 +199,6 @@
 </section> <!-- /section#alliedMaterialsArea -->
 
 <section id="notesArea">
-  <?php /* Notes are only shown to user when logged in as administrator or editor. Customized for Meister Eckhart Archiv.*/ ?>
-  <?php if ($sf_user->hasCredential(array('administrator', 'editor'), false)): ?>
 
   <?php if (check_field_visibility('app_element_visibility_isad_notes_area')): ?>
     <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), '<h2>'.__('Notes area').'</h2>', array($resource, 'module' => 'informationobject', 'action' => 'edit'), array('anchor' => 'notesArea', 'title' => __('Edit notes area'))) ?>
@@ -215,8 +213,6 @@
   <div class="alternativeIdentifiers">
     <?php echo get_partial('informationobject/alternativeIdentifiersIndex', array('resource' => $resource)) ?>
   </div>
-
-  <?php endif;  ?>
 
 </section> <!-- /section#notesArea -->
 
